edd9ad7998e553f8be3dc45ff80ce3e31447f589
XNOVAZZ <chawakitkongkaew@gmail.com>
2026-03-15 17:43:26 +0700

    Delete monkey_trouble.py

--- monkey_trouble.py
+++
@@ -1,5 +0,0 @@
-def monkey_trouble(a_smile, b_smile):
-  WAIT = False # WAIT stand for we are in Trouble
-  if (a_smile is True and b_smile is True) or (a_smile is False and b_smile is False) :
-    WAIT = True
-  return WAIT
